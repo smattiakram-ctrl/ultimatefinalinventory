a9c0e2f379276048e02801b62cec5c6e6f4be779
smattiakram-ctrl <smattiakram@gmail.com>
2026-03-31 23:19:11 +0200

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
+import { Home } from './pages/Home';
+import { Categories } from './pages/Categories';
+import { CategoryProducts } from './pages/CategoryProducts';
+import { Sell } from './pages/Sell';
+import { Sales } from './pages/Sales';
+import { AddProduct } from './pages/AddProduct';
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="categories" element={<Categories />} />
+          <Route path="categories/:id" element={<CategoryProducts />} />
+          <Route path="sell" element={<Sell />} />
+          <Route path="sales" element={<Sales />} />
+          <Route path="add-product" element={<AddProduct />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
